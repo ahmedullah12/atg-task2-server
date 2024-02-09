c952f4afa19fb365d0f96f69db0811f308c8a545
ahmedullah12 <gulamjoy321@gmail.com>
2024-02-09 10:34:41 +0600

    add a post method added

--- routes/posts.js
+++ routes/posts.js
@@ -14,5 +14,23 @@ router.get('/posts', async(req, res) => {
     }
 });
 
+router.post('/posts', async (req, res) => {
+    try {
+        const postData = req.body;
+        if (!postData.desc || !postData.userEmail || !postData.userId || !postData.userName) {
+            return res.status(400).json({ status: false, message: 'Incomplete post data' });
+        }
+
+
+        const newPost = new Post(postData);
+        const savedPost = await newPost.save();
+
+        res.json({ status: true, message: "Post has been added." });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: false, message: 'Internal server error' });
+    }
+});
+
 export {router as PostRouter}
 
