c2329e6317d03f42872d8e6a6bd3add905059906
ahmedullah12 <gulamjoy321@gmail.com>
2024-02-09 15:13:03 +0600

    added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: "http://localhost:3000",
     credentials: true,
 }));
 app.use(cookieParser());
